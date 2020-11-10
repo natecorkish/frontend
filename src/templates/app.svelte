25b8010ae54b7b0a02f011ee78065a07d3b92860
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2020-11-10 13:20:13 +0000

    Create app.svelte

---
+++ src/templates/app.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    <link rel="stylesheet" href="assets/css/app.css">
+</svelte:head>
+
+<script>
+    import Router from "svelte-spa-router";
+    import routes from "../../routes";
+</script>
+
+
+<nav class="p-2 bg-gray-200 flex justify-between">
+    <div>LOGO</div>
+    <div>LINKS</div>
+</nav>
+
+<main>
+    <Router {routes} />
+</main>
